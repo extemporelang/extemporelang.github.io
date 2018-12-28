271d56d0f8c0e09f6ca615edd8fea9e672c069e2
Yuya Chiba <cy.blue.9@gmail.com>
2018-12-29 01:12:26 +0900

    Clean default search function

--- js/main.js
+++ js/main.js
@@ -12,20 +12,7 @@ $(function() {
         prefetch: baseurl + '/search.json'
     });
 
-    $('#search-box').typeahead({
-        minLength: 0,
-        highlight: true
-    }, {
-        name: 'pages',
-        display: 'title',
-        source: pages
-    });
-
-    $('#search-box').bind('typeahead:select', function(ev, suggestion) {
-        window.location.href = suggestion.url;
-    });
-
-
+    
     // Markdown plain out to bootstrap style
     $('#markdown-content-container table').addClass('table');
     $('#markdown-content-container img').addClass('img-responsive');
